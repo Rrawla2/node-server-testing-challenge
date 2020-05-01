521fe1e0c3fcea29930b0cd5786ac1654bfecdaa
Kristi Gribble <kgribble4083@gmail.com>
2020-04-30 20:27:20 -0500

    retested delete function

--- hobbits.spec.js
+++ hobbits.spec.js
@@ -2,15 +2,15 @@ const db = require("./data/db-config.js")
 
 const Hobbits = require("./hobbits-model.js")
 
-beforeEach(async () => {
-    await db("hobbits").truncate()
-})
-
 describe("hobbits model", () => {
     describe("insert()", () => {
+        beforeEach(async () => {
+            await db("hobbits").truncate()
+        })
+
         it("should insert the hobbit into the database", async () => {
-            await Hobbits.insert({ name: "Pippin" })
-            await Hobbits.insert({ name: "Frodo" })
+            await Hobbits.insert({ name: "Merry" })
+            await Hobbits.insert({ name: "Bilbo" })
 
             const hobbits = await db("hobbits")
 
@@ -31,11 +31,11 @@ describe("hobbits model", () => {
             await Hobbits.remove({ id: 1 })
 
             const hobbits = await db("hobbits")
-            expect(hobbits).toHaveLength(0)
+            expect(hobbits).toHaveLength(2)
         })
 
         it("should remove the hobbit from the database", async () => {
-            await Hobbits.remove({ id: 1 })
+            await Hobbits.remove({ id: 2 })
 
             const hobbits = await db("hobbits")
             expect(hobbits).toHaveLength(5)
